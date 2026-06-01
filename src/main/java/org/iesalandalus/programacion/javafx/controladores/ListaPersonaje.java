a3458e5571d0618097022210f8eb8603edd286b9
Saroagui813 <saroagui8132006@gmail.com>
2026-06-02 00:18:18 +0200

    Añado botones de contar, borrar y deshacer borrado

--- src/main/java/org/iesalandalus/programacion/javafx/controladores/ListaPersonaje.java
+++ src/main/java/org/iesalandalus/programacion/javafx/controladores/ListaPersonaje.java
@@ -1,7 +1,9 @@
 package org.iesalandalus.programacion.javafx.controladores;
 
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import org.iesalandalus.programacion.javafx.csv.Personaje;
@@ -11,11 +13,55 @@ import java.util.List;
 
 public class ListaPersonaje extends Controlador {
 
+    // Estos NO llevan @FXML porque no son componentes de la vista,
+    // son "memoria" interna para recordar el último borrado.
+    private Personaje ultimoBorrado;
+    private int ultimaPosicion;
+
+    @FXML
+    private Button botonBorrar;
+
+    @FXML
+    private Button botonDeshacer;
+
+    @FXML
+    private Button botonContar;
+
     @FXML
     private Label idLabel;
 
     @FXML
     private ListView<Personaje> idLista;
+
+    @FXML
+    void borrarPersonaje(ActionEvent event) {
+        Personaje seleccionado = idLista.getSelectionModel().getSelectedItem();
+        if (seleccionado != null) {
+            ultimaPosicion = idLista.getSelectionModel().getSelectedIndex();
+            ultimoBorrado = seleccionado;
+            idLista.getItems().remove(seleccionado);
+            idLabel.setText("Personaje borrado");
+        } else  {
+            idLabel.setText("Seleccione un personaje");
+        }
+    }
+
+    @FXML
+    void deshacerBorrar(ActionEvent event) {
+        if (ultimoBorrado != null) {
+            idLista.getItems().add(ultimaPosicion, ultimoBorrado);
+            idLabel.setText("Personaje restaurado.");
+            ultimoBorrado = null;
+        } else {
+            idLabel.setText("No hay nada que deshacer.");
+        }
+    }
+
+    @FXML
+    void contarPersonajes(ActionEvent event) {
+        int total = idLista.getItems().size();
+        idLabel.setText("Hay " + total + " personajes");
+    }
 
     // Recibe la lista de personajes desde el Main y la mete en el ListView.
     // getItems() = los datos de la lista. setItems() = asignar esos datos.
