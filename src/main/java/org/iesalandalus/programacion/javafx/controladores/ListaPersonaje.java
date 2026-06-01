662d66f78895db3b95ae6d0085363cde98682574
Saroagui813 <saroagui8132006@gmail.com>
2026-06-01 23:37:24 +0200

    Comentarios en ListaPersonaje para entender el código escritos

--- src/main/java/org/iesalandalus/programacion/javafx/controladores/ListaPersonaje.java
+++ src/main/java/org/iesalandalus/programacion/javafx/controladores/ListaPersonaje.java
@@ -17,10 +17,21 @@ public class ListaPersonaje extends Controlador {
     @FXML
     private ListView<Personaje> idLista;
 
+    // Recibe la lista de personajes desde el Main y la mete en el ListView.
+    // getItems() = los datos de la lista. setItems() = asignar esos datos.
+    // FXCollections.observableArrayList(...) convierte la List normal en
+    // ObservableList (la única que acepta el ListView).
     public void setPersona(List<Personaje> personas){
         idLista.setItems(FXCollections.observableArrayList(personas));
     }
 
+    // idLista                  -> el ListView
+    // .getSelectionModel()     -> el GESTOR que sabe qué elemento está seleccionado (NO los datos; los datos son getItems())
+    // .selectedItemProperty()  -> la "caja" observable con el elemento seleccionado
+    // .addListener(...)        -> "avísame cada vez que cambie la selección"
+    //   observable           -> la propiedad que cambió (se ignora)
+    //   personajeAnterior    -> el que estaba seleccionado ANTES
+    //   personajeSeleccionado-> el que está seleccionado AHORA (el que nos interesa)
     public void initialize(){
         idLista.getSelectionModel().selectedItemProperty().addListener((observable, personajeAnterior, personajeSeleccionado) -> {
                     if (personajeSeleccionado != null) {
@@ -29,6 +40,7 @@ public class ListaPersonaje extends Controlador {
         });
     }
 
+    // Escribe en el Label el texto que le pasemos.
     private void mostrarTexto(Personaje personaje) {
         idLabel.setText("Has seleccionado el personaje: " + personaje.getNombre());
     }
